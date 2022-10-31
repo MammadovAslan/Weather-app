993b106daa45c211589737c6dfcaddd55475d196
Aslan Mammadov <mammadovaslan21@gmail.com>
2022-10-31 20:01:54 +0400

    reallife information added

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
-const resolve = (position) => {
-  //*---------------------------Weather API-----------------------------
-  const request = fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=b9e1392e3720d63b937ad2a6e1142a1e`
-  );
+setInterval(() => {
+  const resolve = (position) => {
+    //*---------------------------Weather API-----------------------------
+    const request = fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=b9e1392e3720d63b937ad2a6e1142a1e`
+    );
 
-  const now = new Date(position.timestamp);
-  const hours = now.getHours() > 9 ? `${now.getHours()}` : `0${now.getHours()}`;
-  const minutes = now.getMinutes() > 9 ? `${now.getMinutes()}` : `0${now.getMinutes()}`;
+    request
+      .then((response) => {
+        return response.json();
+      })
+      .then((resp) => {
+        const container = document.querySelector(".container");
+        const now = new Date(position.timestamp);
 
-  request
-    .then((response) => {
-      return response.json();
-    })
-    .then((resp) => {
-      console.log(resp);
-      const container = document.querySelector(".container");
-      container.innerHTML = `
+        const hours = now.getHours() > 9 ? `${now.getHours()}` : `0${now.getHours()}`;
+        const minutes = now.getMinutes() > 9 ? `${now.getMinutes()}` : `0${now.getMinutes()}`;
+        const seconds = now.getSeconds() > 9 ? `${now.getSeconds()}` : `0${now.getSeconds()}`;
+
+        let time = `${hours}:${minutes}:${seconds}`;
+        container.innerHTML = `
       <main>
       <div id="temp">
       <p class="degrees">${`${Math.floor(resp.main.temp)}<span>&#176;</span>`}</p>
         </div>
         <div id="location">
         <h3 id="city">${resp["name"]}</h3>
-        <p class="date">${`${hours}:${minutes} -  ${new Date().toLocaleDateString()}`}</p>
+        <p class="date">${`${time} -  ${new Date().toLocaleDateString()}`}</p>
         </div>
         </main>
         <aside>
@@ -41,28 +44,28 @@ const resolve = (position) => {
         </aside>
         `;
 
-      //*Code bellow will change background depending on current time
-      // let test = 6 //<- to test it variable 'hours' may be replaced with 'test'
+        //*Code bellow will change background depending on current time
+        // let test = 6 //<- to test it variable 'hours' may be replaced with 'test'
 
-      if (hours >= 5 && hours < 18) {
-        document.body.className = "";
-        document.body.classList.add("day");
-      } else if (hours >= 18 && hours < 21) {
-        document.body.className = "";
-        document.body.classList.add("sunset");
-      } else {
-        document.body.className = "";
-        document.body.classList.add("night");
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
+        if (hours >= 5 && hours < 18) {
+          document.body.className = "";
+          document.body.classList.add("day");
+        } else if (hours >= 18 && hours < 21) {
+          document.body.className = "";
+          document.body.classList.add("sunset");
+        } else {
+          document.body.className = "";
+          document.body.classList.add("night");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const reject = (error) => {
+    console.log(error);
+  };
 
-const reject = (error) => {
-  console.log(error);
-};
-
-//*----------------Request location------------------------
-navigator.geolocation.getCurrentPosition(resolve, reject);
+  //*----------------Request location------------------------
+  navigator.geolocation.getCurrentPosition(resolve, reject);
+}, 1000);
